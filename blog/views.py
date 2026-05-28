fdc0c724da44a9368d16b9d3407d3adfe463720b
Sophia Vikenteva <vikentevasof@gmail.com>
2026-05-28 20:58:10 +0300

    Added Blogy Bootstrap UI

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,28 @@ from django.shortcuts import render
 
 def home(request):
 
+    posts = [
+        {
+            'title': 'First Blog Post',
+            'content': 'This is our first Django blog platform article.',
+            'author': 'Sofia'
+        },
+
+        {
+            'title': 'Bootstrap Integration',
+            'content': 'We connected Blogy template to Django.',
+            'author': 'Alona'
+        },
+
+        {
+            'title': 'Python Django',
+            'content': 'Our project is finally working.',
+            'author': 'Team'
+        }
+    ]
+
     return render(
         request,
-        'home.html'
+        'home.html',
+        {'posts': posts}
     )
